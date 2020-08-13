66f6e4482d1a8fffb0150071b0348e70c432713d
590610676 <achirawit_run@cmu.ac.th>
2020-08-13 16:08:09 +0700

    Update App.vue
    change1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,100 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <br /><br />
+    <div id="home">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-1" label="FristName:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.fristname"
+            required
+            placeholder="FristName"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="LastName:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.lastname"
+            required
+            placeholder="LastName"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="input-group-3"
+          label="Email address:"
+          label-for="input-3"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <label for="datepicker-full-width">Birthday</label>
+        <b-form-datepicker
+          id="datepicker-full-width"
+          v-model="form.birthday"
+          menu-class="w-100"
+          calendar-width="100%"
+          class="mb-2"
+        ></b-form-datepicker>        
+        <b-button type="submit" variant="primary" style="margin-bottom:10px;">Add employee</b-button>
+      </b-form>
+
+      <div>
+        <b-table striped hover :items="items"></b-table>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { Firtname: "", Lastname: "", Birthday: "", Email: "", Operation: "" }
+      ],
+      form: {
+        firtname: "",
+        Lastname: "",
+        email: "",
+        birthday: ""
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
